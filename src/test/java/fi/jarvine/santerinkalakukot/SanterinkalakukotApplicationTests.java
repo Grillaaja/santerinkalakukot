e1713166191310b41a511f27e88f6b199b1c331e
Grillaaja <santeri.jarvenpaa22@gmail.com>
2020-10-15 21:40:57 +0300

    Created the project and added some needed dependencies

---
+++ src/test/java/fi/jarvine/santerinkalakukot/SanterinkalakukotApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.jarvine.santerinkalakukot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanterinkalakukotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
